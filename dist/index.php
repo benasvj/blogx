7d0736ef15cd71a782a1aa46fab9e6bc6e1c6f99
benasvj <bvajega@gmail.com>
2018-01-23 23:41:52 +0200

    secondCommit

--- dist/index.php
+++ dist/index.php
@@ -2,8 +2,29 @@
 <html lang="en">
 <?php include('parts/head.php') ?>
 <body>
+<section class="main-image">
 <?php include('parts/header.php') ?>
+  <div class="left-arrow arrow">
+    <i class="fa fa-chevron-left" aria-hidden="true"></i>
+  </div>
+  <div class="right-arrow arrow">
+    <i class="fa fa-chevron-right" aria-hidden="true"></i>
+  </div>
+  <div class="main-content">
+    <h1>We don't care about your business</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolorem, ducimus eius expedita impedit perferendis provident quia quod tenetur voluptas
+    </p>
+    <div class="button">
+      about us
+    </div>
+  </div>
+</section>
 
+<section class="features">
+  <div class="container">
+
+  </div>
+</section>
 
   <script src="script.js"></script>
 </body>
--- dist/parts/header.php
+++ dist/parts/header.php
@@ -1,21 +1,42 @@
 <header>
   <div class="contacts-menu">
-    <div class="social">
-      <a href="https://facebook.com">
+    <div class="container">
+      <div class="social">
+        <a href="https://facebook.com">
         <i class="fa fa-facebook" aria-hidden="true"></i>
-      </a>
-    </div>
-    <div class="contacts">
-      <i class="fa fa-facebook" aria-hidden="true"></i>
-      <p>laikas</p>
-      <i class="fa fa-facebook" aria-hidden="true"></i>
-      <a href="tel:+370898888">+370 89 8888</a>
-      <i class="fa fa-facebook" aria-hidden="true"></i>
-      <a href="mailto:info@google.com">info@google.com</a>
+        </a>
+        <a href="https://facebook.com">
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="https://facebook.com">
+        <i class="fa fa-linkedin" aria-hidden="true"></i>
+        </a>
+        <a href="https://facebook.com">
+        <i class="fa fa-google-plus" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div class="contacts">
+        <i class="fa fa-clock-o" aria-hidden="true"></i>
+        <p>laikas</p>
+        <i class="fa fa-phone" aria-hidden="true"></i>
+        <a href="tel:+370898888">+370 89 8888</a>
+        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+        <a href="mailto:info@google.com">info@google.com</a>
+      </div>
     </div>
   </div>
   <div class="navigation-menu">
-
+    <div class="container">
+      <img src="img/logo.png" alt="logo">
+      <nav>
+        <a href="">home</a>
+        <a href="">about us</a>
+        <a href="">pages</a>
+        <a href="">projects</a>
+        <a href="">blog</a>
+        <a href="">contact us</a>
+      </nav>
+    </div>
   </div>
 
 </header>
