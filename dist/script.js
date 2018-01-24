b2a614352fe8198c47d126f04e718de970f43dc8
benasvj <bvajega@gmail.com>
2018-01-24 14:49:48 +0200

    thirdCommit

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,22 @@
-var shape =document.querySelector('.shape');
-var btn = document.querySelector('button');
+const html ={
+  backward:document.querySelector(".fa-chevron-left"),
+  forward:document.querySelector(".fa-chevron-right"),
+  home:document.querySelector("section.main-image")
+}
 
-btn.addEventListener('click', function () {
-  console.log(shape.value)
+
+var current = 0;
+var photos=["img/index/background1.jpeg", "img/index/background2.jpeg", "img/index/background3.jpeg"]
+
+
+html.forward.addEventListener('click', function () {
+  html.home.style.backgroundImage=`url(${photos[current]}`;
+  current++;
+  if(!photos[current]) current=0;
 });
+
+html.backward.addEventListener("click", function () {
+  html.home.style.backgroundImage=`url(${photos[current]})`;
+  current--;
+  if(current<0) current=photos.length-1;
+})
