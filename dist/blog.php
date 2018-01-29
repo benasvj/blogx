d3798d07e4373403615e223fc0b9d7c84e50e057
benasvj <bvajega@gmail.com>
2018-01-29 17:56:40 +0200

    blogshalfdone

--- dist/blog.php
+++ dist/blog.php
@@ -4,8 +4,117 @@
 <body>
 <?php include "parts/header.php"?>
 
+<section class="current-page">
+    <div class="container">
+        <h3>BLOG</h3>
+        <p>Home &gt Blog</p>
+        <div class="clearfix"></div>
+    </div>
+</section>
 
+<section class="blogs-page">
+    <div class="container">
+        <div class="blogs-left">
+            <div class="wrapper">
+                <div class="blog-photo">
+                    <img src="img/blog/1.jpeg" alt="blog photo">
+                </div>
+                <div class="blog-date">
+                    <p>DATE <br><span>March 24, 2015</span></p>
+                </div>
+                <div class="blog-title">
+                    <h2>Steps Toward Developing A Content Strategy</h2>
+                </div>
+                <div class="blog-text">
+                    <p><span>Posted by: Mastia</span></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis ea est illum ipsa itaque liberop tiovolup Conseq uuntur deserunt expedita, fugiat hic illum porro quidem quis recusandae vero? Aliquiassumenda cum delectus eaque eligendi, enim eum excepturi fugit illum impedit in iste laudantium modi natus, nisi nobis gfy  g obcaecati praesentium quis reiciendis rerum sapiente</p>
+                    <p>Tumblr literally kogi deep v. Bespoke migas four dollar toast, raw denim gluten-free beard hella cold-pressed lumbersexual vinyl biodiesel scenester lomo fanny pack. Sustainable stumptown street art, +1 Etsy gentrify fashion axe sriracha meh farm-to-table Godard hashtag lo-fi aesthetic irony. Disrupt tofu mlkshk, sartorial Pitchfork retro heirloom ugh PBRB actually ethical. YOLO roof party lomo, cardigan health goth kitsch.</p>
+                    <p>DIY food truck Carles aesthetic, sartorial keytar gentrify Neutra synth kogi yr McSweeney’s. Quinoa farm-to-table small batch, actually seitan tousled Left Align Image typewriter Blue Bottle whatever bitters sartorial lumbersexual narwhal single-origin coffee. Salvia Tumblr street art, art party cronut freegan post-ironic forage photo booth mixtape Vice Brooklyn Schlitz ugh wolf.</p>
+                    <p>Fingerstache Intelligentsia you probably haven’t heard of them, tilde mustache yr McSweeney’s butcher kale chips Bushwick. Schlitz 3 wolf moon mustache, cold-pressed cred irony Williamsburg locavore small batch umami polaroid photo booth cronut Pitchfork gluten-free. Tofu bitters forage food truck 90’s Pinterest.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, voluptatem optio quasi labore consequuntur repellendus perspiciatis error ipsa fuga. Quasi veniam.Echo Park pork belly irony tilde organic Truffaut wayfarers. Leggings Truffaut ugh, PBRB mixtape 3 wolf moon Schlitz. Cray biodiesel cred, locavore listicle cronut 3 wolf moon small batch paleo</p>
+                    <p>Tumblr literally kogi deep v. Bespoke migas four dollar toast, raw denim gluten-free beard hella cold-pressed lumbersexual vinyl biodiesel scenester lomo fanny pack. Sustainable stumptown street art, +1 Etsy gentrify fashion axe sriracha meh farm-to-table Godard hashtag lo-fi aesthetic irony. Disrupt tofu mlkshk, sartorial Pitchfork retro heirloom ugh PBRB actually ethical. YOLO roof party lomo, cardigan health goth kitsch.</p>
+                    <p>DIY food truck Carles aesthetic, sartorial keytar gentrify Neutra synth kogi yr McSweeney’s. Quinoa rm-to-table small batch, actually seitan tousled Left Align Image typewriter Blue Bottle whatever bittesartorial lumbersexual narwhal single-origin coffee. Salvia Tumblr street art, art party cronut freegan post-ironic forage photo booth mixtape Vice Brooklyn Schlitz ugh wolf.</p>
+                    <p>psum dolor sit amet, consectetur adipisicing elit. Dolorum, voluptatem optio quasi labore consequuntur pellendus perspiciatis error ipsa fuga. Quasi veniam.Echo Park pork belly irony tilde organic Truffawayfarers. Leggings Truffaut ugh, PBRB mixtape 3 wolf moon Schlitz. Cray biodiesel cred, locavore listicle cronut 3 wolf moon small batch paleo</p>
+                    <p>Tumblr literally kogi deep v. Bespoke migas four dollar toast, raw denim gluten-free beard hella cold-pressed lumbersexual vinyl biodiesel scenester lomo fanny pack.</p>
+                </div>
+            </div>
+        </div>
 
+        <div class="blogs-right">
+            <div class="input-container">
+                <input id="searchinput" style="text-indent:17px;" type="text" placeholder="Searching">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+            <div class="newposts">
+                <h2>recent posts</h2>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/1.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>There is no good blog without tim great readability is very importent</p>
+                        <p><span>Viral polaroid ugh</span></p>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/2.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>6 Reasons to Attend Elevate New York 2015 events one way oft the development</p>
+                        <p><span>labitur qui</span></p>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/3.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>User Experience As A Ranking Signal Is  Currently Only For Mobile Results,</p>
+                        <p><span>Viral polaroid ugh</span></p>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/4.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>When you start with what’s at stake for the  right to their attention</p>
+                        <p><span>Ei his nonumes</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="latestprojects">
+                <h2>latest projects</h2>
+                <div class="latestprojects-block">
+                    <div class="projectx">
+                        <img src="img/projects/2.jpeg" alt="project photo">
+                    </div>
+                    <div class="projectx">
+                        <img src="img/projects/4.jpeg" alt="project photo">
+                    </div>
+                    <div class="projectx">
+                        <img src="img/projects/5.jpeg" alt="project photo">
+                    </div>
+                    <div class="projectx">
+                        <img src="img/projects/6.jpeg" alt="project photo">
+                    </div>
+                </div>
+            </div>
+            <div class="blog-categories">
+                <h2>blog categories</h2>
+                <ul>
+                    <li>Building</li>
+                    <li>Company</li>
+                    <li>Design</li>
+                    <li>Press</li>
+                    <li>Tips</li>
+                </ul>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+
+    </div>
+</section>
 
 <?php include "parts/footer.php"?>
 </body>
