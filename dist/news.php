efd5a0757c6d49cdef476799942e553b3f09deab
benasvj <bvajega@gmail.com>
2018-01-29 06:42:48 +0200

    newsDone

--- dist/news.php
+++ dist/news.php
@@ -4,16 +4,143 @@
 <body>
 <?php include "parts/header.php"?>
 
-<section class="stripe">
+<section class="current-page">
     <div class="container">
-        <h2>blog</h2>
-        <h3>home blog</h3>
+        <h3>BLOG</h3>
+        <p>Home &gt Blog</p>
+        <div class="clearfix"></div>
     </div>
 </section>
 
-<section class="blog-post">
+<section class="blogs-page">
     <div class="container">
-        
+        <div class="blogs-left">
+            <div class="wrapper">
+                <div class="blog-photo">
+                    <img src="img/blog/1.jpeg" alt="blog photo">
+                </div>
+                <div class="blog-date">
+                    <p>DATE <br><span>March 24, 2015</span></p>
+                </div>
+                <div class="blog-title">
+                    <h2>Steps Toward Developing A Content Strategy</h2>
+                </div>
+                <div class="blog-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis ea est illum ipsa itaque liberop tiovolup Conseq uuntur deserunt expedita, fugiat hic illum porro quidem quis recusandae vero? Aliquiassumenda cum delectus eaque eligendi, enim eum excepturi fugit illum impedit in iste laudantium modi natus, nisi nobis gfy  g obcaecati praesentium quis reiciendis rerum sapiente […]</p>
+                </div>
+                <div class="read-more">read more</div>
+            </div>
+            <div class="wrapper">
+                <div class="blog-photo">
+                    <img src="img/blog/2.jpeg" alt="blog photo">
+                </div>
+                <div class="blog-date">
+                    <p>DATE <br><span>March 24, 2015</span></p>
+                </div>
+                <div class="blog-title">
+                    <h2>Looking for Developers? Try This Geeky Community</h2>
+                </div>
+                <div class="blog-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis ea est illum ipsa itaque liberop tiovolup Conseq uuntur deserunt expedita, fugiat hic illum porro quidem quis recusandae vero? Aliquiassumenda cum delectus eaque eligendi, enim eum excepturi fugit illum impedit in iste laudantium modi natus, nisi nobis gfy  g obcaecati praesentium quis reiciendis rerum sapiente […]</p>
+                </div>
+                <div class="read-more">read more</div>
+            </div>
+            <div class="wrapper">
+                <div class="blog-photo">
+                    <img src="img/blog/3.jpeg" alt="blog photo">
+                </div>
+                <div class="blog-date">
+                    <p>DATE <br><span>March 24, 2015</span></p>
+                </div>
+                <div class="blog-title">
+                    <h2>How to Spend Less Than an Hour a Day on Email Marketing</h2>
+                </div>
+                <div class="blog-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis ea est illum ipsa itaque liberop tiovolup Conseq uuntur deserunt expedita, fugiat hic illum porro quidem quis recusandae vero? Aliquiassumenda cum delectus eaque eligendi, enim eum excepturi fugit illum impedit in iste laudantium modi natus, nisi nobis gfy  g obcaecati praesentium quis reiciendis rerum sapiente […]</p>
+                </div>
+                <div class="read-more">read more</div>
+            </div>
+            <div class="page-turn">
+                <div class="turning-number">1</div>
+                <div class="turning-number">2</div>
+                <div class="next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+            </div>
+        </div>
+
+        <div class="blogs-right">
+            <div class="input-container">
+                <input id="searchinput" style="text-indent:17px;" type="text" placeholder="Searching">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+            <div class="newposts">
+                <h2>recent posts</h2>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/1.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>There is no good blog without tim great readability is very importent</p>
+                        <p><span>Viral polaroid ugh</span></p>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/2.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>6 Reasons to Attend Elevate New York 2015 events one way oft the development</p>
+                        <p><span>labitur qui</span></p>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/3.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>User Experience As A Ranking Signal Is  Currently Only For Mobile Results,</p>
+                        <p><span>Viral polaroid ugh</span></p>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="postimage">
+                        <img src="img/newposts/4.jpeg" alt="">
+                    </div>
+                    <div class="postinfo">
+                        <p>When you start with what’s at stake for the  right to their attention</p>
+                        <p><span>Ei his nonumes</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="latestprojects">
+                <h2>latest projects</h2>
+                <div class="latestprojects-block">
+                    <div class="projectx">
+                        <img src="img/projects/2.jpeg" alt="project photo">
+                    </div>
+                    <div class="projectx">
+                        <img src="img/projects/4.jpeg" alt="project photo">
+                    </div>
+                    <div class="projectx">
+                        <img src="img/projects/5.jpeg" alt="project photo">
+                    </div>
+                    <div class="projectx">
+                        <img src="img/projects/6.jpeg" alt="project photo">
+                    </div>
+                </div>
+            </div>
+            <div class="blog-categories">
+                <h2>blog categories</h2>
+                <ul>
+                    <li>Building</li>
+                    <li>Company</li>
+                    <li>Design</li>
+                    <li>Press</li>
+                    <li>Tips</li>
+                </ul>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+
     </div>
 </section>
 
