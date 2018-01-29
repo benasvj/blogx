56c547bd46bc9687cc7d5b852617792e6edba094
benasvj <bvajega@gmail.com>
2018-01-29 07:21:08 +0200

    contactsBeMapDone

--- dist/contacts.php
+++ dist/contacts.php
@@ -4,6 +4,27 @@
 <body>
 <?php include "parts/header.php"?>
 
+<section class="current-page">
+    <div class="container">
+        <h3>CONTACTS</h3>
+        <p>Home &gt Contact us</p>
+        <div class="clearfix"></div>
+    </div>
+</section>
+
+<section class="contacts-page">
+    <div class="container">
+        <h2>contact us</h2>
+        <div class="contact-form">
+            <input class="forma" type="text" placeholder="First Name">
+            <input class="forma" type="text" placeholder="Last Name">
+            <input class="forma" type="text" placeholder="Your E-Mail">
+            <input class="forma" type="text" placeholder="Website">
+            <textarea name="textarea" rows="10" cols="50" placeholder="Your Message" class="messagebox"></textarea>
+        </div>
+        <div class="button">send</div>
+    </div>
+</section>
 
 
 
